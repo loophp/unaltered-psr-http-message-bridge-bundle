598d7e336ba10b193ccdc517f53dae768f15c0e1
Pol Dellaiera <pol.dellaiera@protonmail.com>
2020-06-12 22:05:35 +0200

    Update tests.

--- spec/loophp/UnalteredPsrHttpMessageBridgeBundle/Factory/UnalteredPsrHttpFactorySpec.php
+++ spec/loophp/UnalteredPsrHttpMessageBridgeBundle/Factory/UnalteredPsrHttpFactorySpec.php
@@ -8,8 +8,12 @@ use loophp\UnalteredPsrHttpMessageBridgeBundle\Factory\UnalteredPsrHttpFactory;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use PhpSpec\ObjectBehavior;
 use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory;
+use Symfony\Bridge\PsrHttpMessage\HttpMessageFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class UnalteredPsrHttpFactorySpec extends ObjectBehavior
 {
@@ -65,6 +69,72 @@ class UnalteredPsrHttpFactorySpec extends ObjectBehavior
     public function it_is_initializable()
     {
         $this->shouldHaveType(UnalteredPsrHttpFactory::class);
+        $this->shouldImplement(HttpMessageFactoryInterface::class);
+    }
+
+    public function it_should_not_do_anything_if_the_uri_does_not_have_parameters(HttpMessageFactoryInterface $httpMessageFactory, Request $symfonyRequest, ServerRequestInterface $psrRequest)
+    {
+        $symfonyRequest = Request::create('http://localhost/api');
+
+        $httpMessageFactory
+            ->createRequest($symfonyRequest)
+            ->willReturn($psrRequest);
+
+        $this->beConstructedWith($httpMessageFactory);
+
+        $this
+            ->createRequest($symfonyRequest)
+            ->shouldReturn($psrRequest);
+
+        $psrRequest
+            ->withQueryParams()
+            ->shouldNotBeCalled();
+    }
+
+    public function it_should_not_take_care_of_the_response(HttpMessageFactoryInterface $httpMessageFactory, Response $symfonyResponse, ResponseInterface $psrResponse)
+    {
+        $randomBody = sha1(microtime());
+
+        $psrResponse
+            ->getStatusCode()
+            ->willReturn(302);
+
+        $psrResponse
+            ->getProtocolVersion()
+            ->willReturn('1.1');
+
+        $psrResponse
+            ->getBody()
+            ->willReturn($randomBody);
+
+        $httpMessageFactory
+            ->createResponse($symfonyResponse)
+            ->willReturn($psrResponse);
+
+        $this->beConstructedWith($httpMessageFactory);
+
+        $this
+            ->createResponse($symfonyResponse)
+            ->shouldReturn($psrResponse);
+
+        $httpMessageFactory
+            ->createResponse($symfonyResponse)
+            ->shouldHaveBeenCalledOnce();
+
+        $this
+            ->createResponse($symfonyResponse)
+            ->getStatusCode()
+            ->shouldEqual(302);
+
+        $this
+            ->createResponse($symfonyResponse)
+            ->getProtocolVersion()
+            ->shouldEqual('1.1');
+
+        $this
+            ->createResponse($symfonyResponse)
+            ->getBody()
+            ->shouldEqual($randomBody);
     }
 
     public function let()
