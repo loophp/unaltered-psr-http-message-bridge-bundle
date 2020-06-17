de2a8a7ca5b83e70dfecfd946a545223835d15be
Pol Dellaiera <pol.dellaiera@protonmail.com>
2020-06-17 08:13:48 +0200

    Rely on league/uri-query-parser to parse URI query string.

--- src/Factory/UnalteredPsrHttpFactory.php
+++ src/Factory/UnalteredPsrHttpFactory.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace loophp\UnalteredPsrHttpMessageBridgeBundle\Factory;
 
-use Generator;
+use League\Uri\Parser\QueryString;
 use Symfony\Bridge\PsrHttpMessage\HttpMessageFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -42,7 +42,7 @@ final class UnalteredPsrHttpFactory implements HttpMessageFactoryInterface
         // Create a new request with the URI and Params updated.
         return $request
             ->withQueryParams(
-                iterator_to_array($this->parseStr($unalteredQueryString))
+                QueryString::extract($unalteredQueryString)
             )
             ->withUri(
                 $request
@@ -57,45 +57,5 @@ final class UnalteredPsrHttpFactory implements HttpMessageFactoryInterface
     public function createResponse(Response $symfonyResponse)
     {
         return $this->httpMessageFactory->createResponse($symfonyResponse);
-    }
-
-    /**
-     * Custom parse_str() function that doesn't alter the parameters key value.
-     *
-     * @see: https://github.com/ecphp/cas-lib/issues/5.
-     *
-     * @param string $queryString
-     *
-     * @return Generator<string, string>
-     */
-    private function parseStr(string $queryString): Generator
-    {
-        parse_str(
-            array_reduce(
-                ['urldecode', 'bin2hex'],
-                static function (string $queryString, callable $callback): string {
-                    return (string) preg_replace_callback(
-                        '/(?<key>[^&=]+?)(?:\[[^&=]*\])?=(?<value>[^&=]*)/u',
-                        static function (array $match) use ($callback): string {
-                            /** @phpstan-ignore-next-line */
-                            return (string) preg_replace(
-                                '/[[:^print:]]/',
-                                '',
-                                str_replace($match['key'], $callback($match['key']), $match[0])
-                            );
-                        },
-                        $queryString
-                    );
-                },
-                $queryString
-            ),
-            $parameters
-        );
-
-        foreach ($parameters as $key => $value) {
-            yield (string) hex2bin((string) $key) => $value;
-        }
-
-        return yield from [];
     }
 }
